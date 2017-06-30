fdfe05e9d14d1ee40914e7cfd03d87783d60bc4a
Ed Hubbell <edhubbell@gmail.com>
2017-06-30 13:53:59 -0400

    first commit

---
+++ Portable/LoggedInPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ComicBook
+{
+    public partial class LoggedInPage : ContentPage
+    {
+
+		public LoggedInPage()
+		{
+			InitializeComponent();
+   		}
+
+		public LoggedInPage(bool loggedIn)
+		{
+			InitializeComponent();
+
+            btnSecureOtherPage.IsEnabled = loggedIn;
+
+            lblBug.IsVisible = loggedIn;
+		}
+
+		void HandleOtherPage_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushModalAsync(new SomeOtherPage());
+        }
+
+		void Back_Clicked(object sender, System.EventArgs e)
+		{
+			Navigation.PopModalAsync();
+		}
+
+    }
+}
---
+++ Portable/SomeOtherPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ComicBook
+{
+    public partial class SomeOtherPage : ContentPage
+    {
+        void Back_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+
+        public SomeOtherPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
